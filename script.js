30e8c644bc36a14be8cc1db973310314b42aed27
YH <yhrj4874@gmail.com>
2026-08-03 03:00:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const checkbox = document.getElementById("checkit");
+const visabtn = document.getElementById("visabtn");
+const mastercard = document.getElementById("mastercard");
+const submit = document.getElementById("submit");
+const Paypal = document.getElementById("Paypal");
+const subresult = document.getElementById("subresult");
+const submitbtn = document.getElementById("submitbtn");
+submit.onclick = function () {
+  if (checkbox.checked) {
+    subresult.textContent = `You are subscribed`;
+  } else {
+    subresult.textContent = `Please subscribe first!`;
+  }
+  if (visabtn.checked) {
+    submitbtn.textContent = `Payment by Visa`;
+  } else if (mastercard.checked) {
+    submitbtn.textContent = `Payment by mastercard`;
+  } else if (Paypal.checked) {
+    submitbtn.textContent = `Payment by Paypal`;
+  } else {
+    submitbtn.textContent = `Choose any of this Card payment!`;
+  }
+};
