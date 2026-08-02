1d7a215706b99b87c83b7662b487701d41e850f4
YH <yhrj4874@gmail.com>
2026-08-03 01:00:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const labelshow = document.getElementById("inputin");
+const btnclick = document.getElementById("submit");
+const resultshow = document.getElementById("result");
+let age;
+btnclick.onclick = function () {
+  age = labelshow.value;
+  age = Number(age);
+
+  if (age == 18) {
+    resultshow.textContent = `You are eligible`;
+  } else if (age <= 0) {
+    resultshow.textContent = `This age doesn't exist`;
+  } else if (age < 18) {
+    resultshow.textContent = `You must be 18+ to apply`;
+  } else if (age <= 95) {
+    resultshow.textContent = `Welcome to our services`;
+  } else if (age <= 200) {
+    resultshow.textContent = `You are too OLD to apply & sorry you are not eligible`;
+  } else {
+    resultshow.textContent = `You should pass the eligibiltity criteria`;
+  }
+};
