ffcaa4938ff6d5ac04fbcb7692a085b024b2a1b2
YH <yhrj4874@gmail.com>
2026-08-07 19:21:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const textbox = document.getElementById("textbox");
+const fahrRadio = document.getElementById("fahr");
+const celRadio = document.getElementById("cel");
+const result = document.getElementById("answer");
+let temp;
+
+function check() {
+  if (fahrRadio.checked) {
+    temp = Number(textbox.value);
+    temp = (temp - 32) / 1.8;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (celRadio.checked) {
+    temp = Number(textbox.value);
+    temp = temp * 1.8 + 32;
+    result.textContent = temp.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Select a Unit";
+  }
+}
